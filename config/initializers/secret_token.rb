85b4ed3c2f8b27bf62099e3501710c154fd700ed
Tyler Dooling <wtdooling@gmail.com>
2012-01-07 01:13:13 -0500

    Created base project with rspec and haml.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GreenBean::Application.config.secret_token = '153908dd9bb0bf43337abd02b520b4b6c6643d15c2f59e401985660e5004866e72ed86bfc5df6ad2260f27f40d21e5fa59571c9ec1f763a6e88d4446e2b09cf9'
